fc469ff7b0899482b4248df74ffa3ec6446e5a60
João Pedro Pleskacznieski Marimon <10726112374@ulife.com.br>
2026-03-24 20:37:02 -0300

    add verification interruption

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -9,24 +9,38 @@
  */
 import javax.swing.*;
 
+import javax.swing.JOptionPane;
+
 public class Principal {
 
     public static void main(String args[]) {
         int i, flag;
-        int numElementos = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de pessoas a ser cadastrado "));
+
+        int numElementos = Integer.parseInt(
+                JOptionPane.showInputDialog("Digite o número de pessoas a ser cadastrado ")
+        );
+
         String vetorPesquisado[] = new String[numElementos];
+
         for (i = 0; i < numElementos; i++) {
             vetorPesquisado[i] = JOptionPane.showInputDialog("Digite o nome para cadastro");
         }
+
         String elementoProcurado = JOptionPane.showInputDialog("Digite o nome a ser procurado");
+
         flag = 0;
-        for (i = 0; i < numElementos; i++) {
+        i = 0;
+
+        while (i < numElementos) {
             if (vetorPesquisado[i].equalsIgnoreCase(elementoProcurado)) {
-                JOptionPane.showMessageDialog(null, "o valor procurado foi encontrado na posição " + i);
+                JOptionPane.showMessageDialog(null,
+                        "o valor procurado foi encontrado na posição " + i);
                 flag = 1;
-            
+                break; // 🔥 interrompe o while
             }
+            i++;
         }
+
         if (flag == 0) {
             JOptionPane.showMessageDialog(null, "o nome não foi encontrado.");
         }
